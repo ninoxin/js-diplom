37c125c6596159b92ee991c1aff8729287d67617
ninoxin <75312516+ninoxin@users.noreply.github.com>
2022-02-14 09:56:06 +0300

    сделала калькулятор

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import sert from './modules/sert'
 import scroll from './modules/scroll'
 import slider from './modules/slider'
 import timer from './modules/timer'
+import calc from './modules/calc'
 
 modal()
 sert()
 scroll()
 slider()
 timer('15 February 2022 21:00')
+calc()
---
+++ src/modules/calc.js
@@ -0,0 +1,33 @@
+const calc = () => {
+    const calcBlock = document.getElementById('calc')
+    const calcType = document.getElementById('calc-type')
+    const calcTypeMaterial = document.getElementById('calc-type-material')
+    const calcInput = document.getElementById('calc-input')
+    const calcTotal = document.getElementById('calc-total')
+
+    const countCalc = () => {
+        const calcTypeValue = +calcType.options[calcType.selectedIndex].value
+        const calcTypeMaterialValue = +calcTypeMaterial.options[calcTypeMaterial.selectedIndex].value
+        const calcInputValue = +calcInput.value
+
+        let totalValue = 0
+
+
+        if (calcType.value && calcTypeMaterial.value && calcInput) {
+
+            totalValue = calcTypeValue * calcTypeMaterialValue * calcInputValue
+
+        } else {
+            totalValue = 0
+        }
+
+        calcTotal.value = totalValue
+    }
+    
+    calcBlock.addEventListener('input', (e) => {
+        if (e.target === calcType || e.target === calcTypeMaterial || e.target === calcInput) {
+            countCalc()
+        }
+    })
+}
+export default calc
